7808ac2ae24ed26adbb28e59e1cf8120e28b569b
Tonie Crumley <tonie.crumley@gmail.com>
2024-10-15 10:14:30 -0500

    Add update and version commands
    The update command will upgrade the version of CAPTURE and the
    project template by pulling the most recent version from the main
    branch.
    
    The version command displays the current version of CAPTURE that is
    installed in the user's environment.

---
+++ commands/update.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+cap_update_description() {
+  cat <<EOF
+  Updates the CAPTURE framework to the latest version.
+EOF
+}
+
+cap_update_help() {
+  cap_update_description
+  echo
+
+  cat <<EOF
+  The "update" command updates CAPTURE to the latest version with all the
+  newest features.
+
+  Usage:
+    cap update
+
+  Example:
+    $ cap update
+    CAPTURE updated to version v0.0.3.
+EOF
+}
+
+cap_update() {
+  # Location where the CAPTURE framework was installed.
+  cap_install_path=$(command -v cap)
+  cap_install_dir=$(dirname "$cap_install_path")
+
+  echo
+
+  # Retrieve the current version of the CAPTURE framework.
+  cd "$cap_install_dir"
+  git checkout main
+  git pull origin main
+
+  # Retrieve the current project template for `cap new`.
+  echo
+  cd project-template
+  git checkout main
+  git pull origin main
+
+  echo
+
+  # Display the current version of CAPTURE.
+  cd ..
+  if current_tag=$(git describe --tags --abbrev=0 2>/dev/null); then
+    echo "CAPTURE updated to version $current_tag."
+  else
+    echo "CAPTURE updated to an unlabeled version."
+  fi
+
+  echo
+}

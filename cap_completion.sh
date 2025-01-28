d7f2c4af7dba2992fbaf1f5e7f676a8e633af924
Tonie Crumley <tonie.crumley@gmail.com>
2025-01-28 10:16:32 -0600

    Replace tabs with spaces

--- cap_completion.sh
+++ cap_completion.sh
@@ -2,30 +2,30 @@
 
 # Function to handle tab completion for CAPTURE
 _cap_completion() {
-	local current subcommand subcommands
-	current="${COMP_WORDS[COMP_CWORD]}"
-	subcommand="${COMP_WORDS[1]}"
-	subcommands="env help md5 new run update version"
+  local current subcommand subcommands
+  current="${COMP_WORDS[COMP_CWORD]}"
+  subcommand="${COMP_WORDS[1]}"
+  subcommands="env help md5 new run update version"
 
   if [[ "${COMP_CWORD}" -eq 1 ]]; then
-		# Show the cap subcommand list.
+    # Show the cap subcommand list.
     mapfile -t COMPREPLY < <(compgen -W "${subcommands}" -- "${current}")
   else
-		case "$subcommand" in
-			"help")
-				# Show the cap subcommand list.
-				if [[ "${COMP_CWORD}" == 2 ]]; then
-					mapfile -t COMPREPLY < <(compgen -W "${subcommands}" -- "${current}")
-				fi
-				;;
-			"md5"|"run")
-				# Tab compete with file and directory names.
-				compopt -o default -o plusdirs
-				;;
-			*)
-				# Don't tab complete.
-				COMPREPLY=()
-		esac
+    case "$subcommand" in
+      "help")
+        # Show the cap subcommand list.
+        if [[ "${COMP_CWORD}" == 2 ]]; then
+          mapfile -t COMPREPLY < <(compgen -W "${subcommands}" -- "${current}")
+        fi
+        ;;
+      "md5"|"run")
+        # Tab compete with file and directory names.
+        compopt -o default -o plusdirs
+        ;;
+      *)
+        # Don't tab complete.
+        COMPREPLY=()
+    esac
   fi
 }
 
